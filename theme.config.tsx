9fd69f1a66edee96ff1c12a200046fbdba6f80c4
jaaaaaaames <63654278+6jtg@users.noreply.github.com>
2023-09-27 16:10:40 +0100

    core units

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,13 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>James' College Notes</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/6jtg/college-notes',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/6jtg/college-notes',
   footer: {
-    text: 'Nextra Docs Template',
+    text: '@jamesgwyer',
   },
 }
 
